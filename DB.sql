ebac129bffff2b3902a934853b98c90753db1598
Sergio González <94931034+Sergioglezzz@users.noreply.github.com>
2024-10-09 13:54:33 +0200

    Create DB.sql

---
+++ DB.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pruebacrud;
+USE pruebacrud;
+
+CREATE TABLE productos(
+  id int NOT NULL AUTO_INCREMENT,
+  nombre varchar(255) NULL,
+  cantidad decimal(5,2) NULL,
+  precio decimal(5,2) NULL,
+  fecha_crear date NULL,
+  fecha_actualizar date NULL,
+  PRIMARY KEY (id)
+);
